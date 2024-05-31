df64ce0a9ece9e3e2d7163b11ea22ef65dfe6dc3
farisfirinciogullari <farisfirinciogullari406@gmail.com>
2024-05-31 19:13:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import random
+sifre = ""
+sayi = int(input("Kaç karakterli bir şifre istersin?\n"))
+for i in range(sayi):
+    sifre += random.choice(karakterler)
+print(sifre)
